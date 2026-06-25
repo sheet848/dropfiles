7d09bc901127a8d45aaa9b18c18498008d7aab56
Sheetal Naik <sheetalnaik848@gmail.com>
2026-06-25 14:40:22 +0530

    Delete lib directory

--- lib/types.ts
+++
@@ -1,9 +0,0 @@
-export type CustomInputProps = {
-    Icon: React.ComponentType;
-    labelTitle: string;
-    labelHtmlFor: string;
-    value?: string;
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    inputName: string;
-    classNames?: string;
-};
