7d5e72dc2c7738d652211ca3d3cbc8542f95f94c
Sheetal Naik <sheetalnaik848@gmail.com>
2026-06-26 17:42:10 +0530

    OTP modal
    the modal section has been completed

---
+++ lib/constants.ts
@@ -0,0 +1,2 @@
+export const HERO_IMAGE = "";
+
---
+++ lib/utils.ts
@@ -0,0 +1,11 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export const isValidEmail = (email: string) => {
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(email);
+}
