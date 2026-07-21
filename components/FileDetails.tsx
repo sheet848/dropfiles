ed9f344c053f60422c44caced01a4c5cca8fc766
Sheetal Naik <sheetalnaik848@gmail.com>
2026-07-21 22:08:48 +0530

    File rename and details display

---
+++ components/FileDetails.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Models } from "node-appwrite"
+import FilePreview from "./FilePreview"
+import { formatDateTime, getFileSize } from "@/lib/utils"
+import { useCallback, useEffect, useState } from "react";
+import { getFileOwnerDetails } from "@/lib/appwrite/file.actions";
+
+const FileDetails = ({ file } : { file: Models.DefaultRow }) => {
+  const [ownerFullName, setOwnerFullName] = useState("");
+
+  const handleFetchOwnerDetails = useCallback(async () => {
+    const user = await getFileOwnerDetails(file.ownerId);
+    console.log("Owneruser:", user);
+    setOwnerFullName(user.fullName);
+  }, [file.ownerId]);
+
+  useEffect(() => {
+    handleFetchOwnerDetails();
+  }, [handleFetchOwnerDetails]);
+
+  const renderItem = (label: string, value: string | number) => {
+    return <div className="mt-4 flex gap-6">
+            <span className="text-gray-500 font-medium w-20">{label}:</span>
+            <span className="uppercase font-medium text-gray-700">{value}</span>
+        </div>
+  }
+
+  return (
+    <div>
+        <FilePreview file={file} />
+        { renderItem("Type", file.extension) }
+        { renderItem("Size", getFileSize(file.size)) }
+        { renderItem("Creator", ownerFullName) }
+        { renderItem("Created", formatDateTime(file.$createdAt)) }
+        { renderItem("Modified", formatDateTime(file.$updatedAt)) }
+    </div>
+  )
+}
+
+export default FileDetails
